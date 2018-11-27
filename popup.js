348d8ecd8648e4f4fd03f101b6b2132e799f11ca
Ryan Davila <ryan.m.davila@gmail.com>
2018-11-27 00:13:44 -0500

    Refactored messages between scripts, adding sync storage

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(popupListener);
 
 chrome.runtime.sendMessage({
-    greeting: "test"
+  greeting: "test"
 });
 
 var expired = false;
@@ -9,14 +9,15 @@ var expired = false;
 var background = chrome.extension.getBackgroundPage();
 
 function popupListener(message, sender, sendResponse) {
-    if (message.greeting == "continue timer" && !expired) {
-          document.getElementById("timer").innerHTML = message.minutes + "m "
-          + message.seconds + "s";
-        }
-    else if (message.greeting == "timePassed") {
-      console.log("end of")
-          expired = true;
-            }
+  if (message.greeting == "continue timer" && !expired) {
+    document.getElementById("timer").innerHTML = message.minutes + "m "
+      + message.seconds + "s";
+  }
+  else if (message.greeting == "timePassed") {
+    console.log("end of")
+    document.getElementById("timer").innerHTML = "EXPIRED";;
+    expired = true;
+  }
 }
 
 if (expired) {
@@ -24,30 +25,3 @@ if (expired) {
   document.getElementById("reset_button").className = 'show';
 
 }
-
-/* var countDownDate = new Date().getTime() + (1000*20)
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    console.log(distance);
-
-    // Time calculations for days, hours, minutes and seconds
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
-    // If the count down is over, write some text
-    if (distance <= 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-        chrome.tabs.insertCSS({
-          file: 'change.css'
-        });
-        audio = new Audio();
-        audio.src = "audio/beep.mp3"
-        audio.play();
-    }
-  }, 1000); **/
