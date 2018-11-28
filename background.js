6e801ac154ed96224a4dd8e20d90bf778ea5299d
Ryan Davila <ryan.m.davila@gmail.com>
2018-11-27 19:21:54 -0500

    Refactored background and content script messaging

--- background.js
+++ background.js
@@ -1,9 +1,25 @@
 var beginningDate = new Date();
 var countdownDate = new Date().getTime() + (1000 * 20);
 var expired = false;
+var visitedYouTube = false;
 
 chrome.runtime.onMessage.addListener(function (message, callback) {
-    if (message.greeting == "timePassed") {
+    if (message.greeting == "checkVisited") {
+        if (visitedYouTube) {
+            sendMessageToCurrentTab({
+                greeting: "visited"
+            });
+            chrome.tabs.insertCSS(null, {
+                file: 'change.css'
+            });
+        }
+        else {
+            sendMessageToCurrentTab({
+                greeting: "notvisited"
+            });
+        }
+    }
+    else if (message.greeting == "timePassed") {
         chrome.tabs.insertCSS(null, {
             file: 'change.css'
         });
@@ -11,17 +27,26 @@ chrome.runtime.onMessage.addListener(function (message, callback) {
         audio.src = "audio/beep.mp3"
         audio.play();
         expired = true;
+        visitedYouTube = true;
     }
+
     else if (message.greeting == "storeTimes") {
         totalTime = parseInt(message.time);
-        if (chrome.storage.sync.get(['totalTime']) == null) {
-            chrome.storage.sync.set({ "totalTime": totalTime }, function () {
-                console.log('Value is set to ' + totalTime);
+        if (chrome.storage.sync.get(['total']) == null) {
+            chrome.storage.sync.set({ ["total"]: totalTime }, function () {
+                alert('Data input');
             });
         } else {
-            chrome.storage.sync.set({ "totalTime": parseInt(chrome.storage.sync.get(['totalTime'])) + totalTime }, function () {
-                console.log('Value increased by ' + totalTime);
+            originalTime = parseInt(chrome.storage.sync.get(['total']));
+            chrome.storage.sync.set({ ["total"]: totalTime + originalTime }, function () {
+                alert('Data updated');
             });
         }
     }
 });
+
+function sendMessageToCurrentTab(msg) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, msg);
+    });
+}
